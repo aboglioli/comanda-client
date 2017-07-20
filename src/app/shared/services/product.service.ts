2d6a7bcafb868875c3aed49cd9b8aaf2c91ee5ef
Alan Boglioli <alan.boglioli@gmail.com>
2017-07-20 02:18:22 -0300

    Add search by name and type in ProductService.get()

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import * as _ from 'lodash';
 
@@ -12,8 +12,18 @@ export class ProductService {
   constructor(private http: Http,
               private notificationService: NotificationService) { }
 
-  get(): Observable<Product[]> {
-    return this.http.get('/products')
+  get({name, type}: {name?: string, type?: string} = {}): Observable<Product[]> {
+    const search = new URLSearchParams();
+
+    if(name) {
+      search.append('name', name);
+    }
+
+    if(type) {
+      search.append('type', type);
+    }
+
+    return this.http.get('/products', {search})
       .map(res => <Product[]>res.json());
   }
 
