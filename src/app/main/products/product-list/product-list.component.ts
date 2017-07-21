4941b6f3ec93b36114863e1fc6b0a227d3e23a58
Alan <alan.boglioli@gmail.com>
2017-07-21 18:38:47 -0300

    Allow saving and modifing products and assign raw products to them

--- src/app/main/products/product-list/product-list.component.ts
+++ src/app/main/products/product-list/product-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as _ from 'lodash';
 
 import { Product } from '../../../models';
 import { ProductService } from '../../../shared/services';
+import { config } from '../../../config';
 
 @Component({
   selector: 'app-product-list',
@@ -11,6 +13,24 @@ import { ProductService } from '../../../shared/services';
 })
 export class ProductListComponent implements OnInit {
   products: Product[];
+  data = [];
+  settings = {
+    mode: 'external',
+    columns: {
+      name: {
+        title: 'Nombre'
+      },
+      price: {
+        title: 'Precio'
+      },
+      type: {
+        title: 'Tipo',
+      },
+      subproducts: {
+        title: 'Subproductos',
+      },
+    }
+  };
 
   constructor(private router: Router,
               private productService: ProductService) { }
@@ -18,11 +38,33 @@ export class ProductListComponent implements OnInit {
   ngOnInit() {
     this.productService.get({type: 'product'}).subscribe((products: Product[]) => {
       this.products = products;
+      this.data =  products.map(product => this.desmaterializeProduct(product));
     });
+
+    // Settings
+    this.settings = _.assign(this.settings, config.ng2SmartTableDefaultSettings);
   }
 
-  goToDetail(productId: string) {
-    this.router.navigate(['/products', productId]);
+  onCreate(event) {
+    this.router.navigate(['/products', 'new']);
+  }
+
+  onEdit(event) {
+    this.router.navigate(['/products', event.data._id]);
+  }
+
+  onDelete() {
+    console.log('delete');
+  }
+
+  private desmaterializeProduct(product: Product): any {
+    return {
+      _id: product._id,
+      name: product.name,
+      price: `\$ ${product.price.value} / ${product.price.quantity.value} ${product.price.quantity.unit}`,
+      type: product.type,
+      subproducts: product.subproducts.length
+    };
   }
 
 }
--- src/app/main/products/product/product.component.ts
+++ src/app/main/products/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as _ from 'lodash';
 
 import { Product, Subproduct } from '../../../models';
 import { ProductService } from '../../../shared/services';
@@ -28,12 +29,16 @@ export class ProductComponent implements OnInit {
           .subscribe((product: Product) => {
             this.product = product;
             this.buildForm(this.product);
+
+            if(this.product.subproducts) {
+              this.calculatePrice(this.product.subproducts);
+            }
           });
       } else {
         this.product = {
-          name: 'Nuevo',
-          description: 'Nuevooo',
-          type: '',
+          name: '',
+          description: '',
+          type: 'single',
           price: {
             value: 0,
             quantity: {
@@ -57,11 +62,34 @@ export class ProductComponent implements OnInit {
 
   onSave() {
     if(this.form.valid) {
+      this.product.type = this.form.value.type;
       this.product.name = this.form.value.name;
       this.product.description = this.form.value.description;
-      this.product.type = this.form.value.type;
 
-      console.log(this.product);
+      const product: Product = _.cloneDeep(this.product);
+      const subproducts = <Subproduct[]>product.subproducts;
+
+      product.subproducts = subproducts.map(subproduct => {
+        return {
+          quantity: {
+            value: +subproduct.quantity.value,
+            unit: subproduct.quantity.unit
+          },
+          product: subproduct.product._id
+        };
+      });
+
+      console.log(product);
+
+      if(product._id) {
+        this.productService.put(product._id, _.omit(product, ['_id', 'price']))
+          .subscribe(product => this.product = product);
+
+        return;
+      }
+
+      this.productService.post(_.omit(product, ['_id', 'price']))
+        .subscribe(product => this.product = product);
     }
 
   }
@@ -82,9 +110,9 @@ export class ProductComponent implements OnInit {
 
   private buildForm(product: Product) {
     this.form = this.fb.group({
-      name: ['', Validators.required],
-      description: [''],
-      type: ['', Validators.required]
+      name: [product.name, Validators.required],
+      description: [product.description],
+      type: [product.type, Validators.required]
     });
   }
 
