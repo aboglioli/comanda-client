d0450dc1e63051a10de81c1d4fbee81897b39041
Alan Boglioli <alan.boglioli@gmail.com>
2017-08-19 04:07:38 -0300

    Subscriptions to Nes (WebSocket) are working properly

--- src/app/shared/services/socket.service.ts
+++ src/app/shared/services/socket.service.ts
@@ -1,24 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject} from 'rxjs';
-
 import * as Nes from 'nes';
+
+import { AccountService } from './account.service';
 
 @Injectable()
 export class SocketService {
   private url = 'http://localhost:3000';
   private client;
 
-  constructor() {
+  constructor(private accountService: AccountService) {
     this.client = new Nes.Client('ws://localhost:3000');
 
-    this.client.connect((err) => {
-      console.log('CONNECTED');
+    const token = this.accountService.getAuthToken();
+
+    this.client.connect({
+      auth: {
+        headers: {
+          authorization: token
+        }
+      }
+    }, (err) => {
+      if(err) {
+        console.log(err);
+      }
+
       this.client.subscribe(
         '/hello',
-        (update, flags) => {
+        (update) => {
           console.log(update);
         },
-        (err) => { }
+        (err) => { console.log(err); }
       );
     });
   }
